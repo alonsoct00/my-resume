f9adc25f651a79f496926456b193cbe2bcea36eb
Alonso CT <g.alonso.ct@gmail.com>
2026-03-02 21:58:40 -0600

    update site english

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-export default {
+import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'node:url'
+
+export default defineConfig({
   site: 'https://alonsoct.dev',
   output: 'static',
-};
+  vite: {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+})
