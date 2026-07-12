797b82fca7423ee82ccf81c4275073a98dec4ee1
Alonso CT <g.alonso.ct@gmail.com>
2026-07-12 15:32:09 -0600

    update alonsoct

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import { fileURLToPath } from 'node:url'
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 
 export default defineConfig({
